c7ecbe9b7fcf68a588d64df5ad7ded8a250a4a47
mohsenmousabi <mohsen4090@gmail.com>
2019-05-31 02:09:53 +0430

    ssr support

--- src/flickity.vue
+++ src/flickity.vue
@@ -5,8 +5,6 @@
 </template>
 
 <script>
-import Flickity from 'flickity';
-
 export default {
   props: {
     options: Object,
@@ -26,6 +24,7 @@ export default {
      * Initialize a new flickity and emit init event.
      */
     init() {
+      const Flickity = require('flickity');
       this.$flickity = new Flickity(this.$el, this.options);
       this.$emit('init', this.$flickity);
     },
@@ -217,6 +216,7 @@ export default {
      * @return {Flickity}
      */
     data() {
+      const Flickity = require('flickity');
       return Flickity.data(this.$el);
     },
 
